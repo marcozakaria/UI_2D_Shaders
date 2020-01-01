fa99f58fd2095ecee509f2d4acbfe6b88930fa8d
Marco Zakaria <marcozakaria@yahoo.com>
2020-01-01 23:53:53 +0200

    Add radial shear posteffect

---
+++ Assets/Shaders/Post effect/RadialBlurEffect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RadialBlurEffect : MonoBehaviour
+{
+    public Material targetMAt;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, targetMAt);
+    }
+}
